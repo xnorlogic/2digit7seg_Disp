b904479f9bd4dbca6380e1544a758e841de7080e
Andy R <xnorlogic@gmail.com>
2018-09-18 12:34:10 -0400

    intial push

---
+++ library/lib/tl_basetypes.h
@@ -0,0 +1,31 @@
+/**************************************************************************************************\
+ *** 
+ *** Simulink model       : TwoDigit_7SegDisp_TL
+ *** TargetLink subsystem : TwoDigit_7SegDisp_TL/TwoDigit_7SegDisp
+ *** Codefile             : tl_basetypes.h
+ ***
+ *** Generation date: 2018-09-18 11:25:13
+ ***
+ *** TargetLink version      : 4.2 from 14-Oct-2016
+ *** Code generator version  : Build Id 4.2.0.26 from 2016-10-17 16:31:48
+
+\**************************************************************************************************/
+
+#ifndef TL_BASETYPES_H
+#define TL_BASETYPES_H
+
+
+typedef unsigned char Bool; /* Description: boolean basetype */
+typedef float Float32; /* Description: 32 bit floating-point basetype */
+typedef double Float64; /* Description: 64 bit floating-point basetype */
+typedef signed short int Int16; /* Description: 16 bit signed integer basetype */
+typedef signed long int Int32; /* Description: 32 bit signed integer basetype */
+typedef signed char Int8; /* Description: 8 bit signed integer basetype */
+typedef unsigned short int UInt16; /* Description: 16 bit unsigned integer basetype */
+typedef unsigned long int UInt32; /* Description: 32 bit unsigned integer basetype */
+typedef unsigned char UInt8; /* Description: 8 bit unsigned integer basetype */
+
+#endif /* TL_BASETYPES_H */
+/*----------------------------------------------------------------------------*\
+  END OF FILE
+\*----------------------------------------------------------------------------*/
---
+++ library/source/tl_types.h
@@ -0,0 +1,23 @@
+/**************************************************************************************************\
+ *** 
+ *** Simulink model       : TwoDigit_7SegDisp_TL
+ *** TargetLink subsystem : TwoDigit_7SegDisp_TL/TwoDigit_7SegDisp
+ *** Codefile             : tl_types.h
+ ***
+ *** Generation date: 2018-09-18 11:25:13
+ ***
+ *** TargetLink version      : 4.2 from 14-Oct-2016
+ *** Code generator version  : Build Id 4.2.0.26 from 2016-10-17 16:31:48
+
+\**************************************************************************************************/
+
+#ifndef TL_TYPES_H
+#define TL_TYPES_H
+
+
+#include "tl_basetypes.h"
+
+#endif /* TL_TYPES_H */
+/*----------------------------------------------------------------------------*\
+  END OF FILE
+\*----------------------------------------------------------------------------*/
